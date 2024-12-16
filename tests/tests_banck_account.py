ed0b5376f2d977d016f7dc1f535829238fbf45ee
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-12-16 19:03:32 -0400

    feat: add test count

--- tests/tests_banck_account.py
+++ tests/tests_banck_account.py
@@ -13,6 +13,10 @@ class BankAccountTests(unittest.TestCase):
         if os.path.exists(self.account.log_file):
             os.remove(self.account.log_file)
 
+    def _count_lines(self, filename):
+        with open(filename) as f:
+            return len(f.readlines())
+
     def test_deposit(self):
         self.account.deposit(100)
         self.assertEqual(self.account.balance, 1100)
@@ -27,3 +31,8 @@ class BankAccountTests(unittest.TestCase):
     def test_transaction_log(self):
         self.account.deposit(100)
         assert os.path.exists("transation_log.txt")
+
+    def test_count_transation(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(100)
+        assert self._count_lines(self.account.log_file) == 2
