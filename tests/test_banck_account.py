be8f903e60dd4d1b8fca18921f28a40f4e55fc51
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-12-22 20:00:58 -0400

    add parametrizacion de pruebas

--- tests/test_banck_account.py
+++ tests/test_banck_account.py
@@ -73,3 +73,16 @@ class BankAccountTests(unittest.TestCase):
         mock_datetime.now.return_value.hour = 18
         with self.assertRaises(WithdrawalTimeRestrictionError):
             self.account.withdraw(100)
+
+    def test_deposit_varios_ammounts(self):
+        tests_cases = [
+            {"ammount": 100, "expected": 1100},
+            {"ammount": 3000, "expected": 4000},
+            {"ammount": 4500, "expected": 5500},
+        ]
+
+        for case in tests_cases:
+            with self.subTest(case=case):
+                self.account = BankAccount(balance=1000, log_file="transations.txt")
+                new_balance = self.account.deposit(case["ammount"])
+                self.assertEqual(new_balance, case["expected"])
